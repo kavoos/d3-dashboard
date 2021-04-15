373a273dfa89101fd0ae18eb94a0866393a88e69
Kavoos Boloorchi <kavoos.boloorchi@biomerieux.com>
2021-04-15 16:40:29 +0200

    Add useWindowSize

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,24 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { Sidebar } from '../sidebar/sidebar'
 import { AppRouter } from '@src/pages/app-router'
 import { BrowserRouter } from 'react-router-dom'
 
 import '@styles/components/app.css'
+import { useWindowSize } from '@src/hooks/use-window-size'
 
-export const App: FC = (): React.ReactElement => (
-  <BrowserRouter>
-    <div className="app">
-      <Sidebar />
-      <AppRouter />
-    </div>
-  </BrowserRouter>
-)
+export const App: FC = () => {
+  const size = useWindowSize()
+
+  useEffect(() => {
+    console.log(`w: ${size.width}px, h: ${size.height}px`)
+  }, [size])
+
+  return (
+    <BrowserRouter>
+      <div className='app'>
+        <Sidebar />
+        <AppRouter />
+      </div>
+    </BrowserRouter>
+  )
+}
